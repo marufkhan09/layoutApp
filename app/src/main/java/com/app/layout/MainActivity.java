bb3a967decf9ee0cec0f77f16308fcbeda038108
maruf09 <maruf@ifarmer.asia>
2024-11-17 00:32:27 +0600

    listview added

--- app/src/main/java/com/app/layout/MainActivity.java
+++ app/src/main/java/com/app/layout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.app.layout;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String[] items = {
+            "Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
+            "Item 6", "Item 7", "Item 8", "Item 9", "Item 10",
+            "Item 11", "Item 12", "Item 13", "Item 14", "Item 15",
+            "Item 16", "Item 17", "Item 18", "Item 19", "Item 20"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        listView = findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
     }
 }
